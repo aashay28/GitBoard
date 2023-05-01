945bf78c37425d61b77f0acab0ae910a3dc77ea6
Foram Sankhavara <foram.sankhavara@ridiscovery.com>
2023-05-01 16:11:04 +0530

    Language selection functionality implementation

---
+++ src/helpers/Constant.jsx
@@ -0,0 +1,7 @@
+export const languageOptions = [
+    { value: '', label: 'Select an option' },
+    { value: 'javascript', label: 'Javascript' },
+    { value: 'python', label: 'Python' },
+    { value: 'react', label: 'React' },
+    { value: 'node', label: 'Node' },
+    { value: 'java', label: 'JAVA' }]
--- src/pages/Dashboard/Projects.jsx
+++ src/pages/Dashboard/Projects.jsx
@@ -1,32 +1,36 @@
 import DefaultLayout from "../../layout/DefaultLayout";
 import CardOne from "../../components/CardOne";
-
+import Spinner from "../../helpers/Spinner";
 import axios from "axios";
 import { useEffect, useState } from "react";
 const Projects = () => {
   const [repositories, setRepositories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+  const [language, setLanguage] = useState('python')
   useEffect(() => {
+    setIsLoading(true)
     axios
       .get(
-        "https://api.github.com/search/repositories?q=language:python+stars:>=100&sort=stars&order=desc"
+        `https://api.github.com/search/repositories?q=language:${language}+stars:>=100&sort=stars&order=desc`
       )
       .then((response) => {
         setRepositories(response.data.items);
+        setIsLoading(false)
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [language]);
 
   return (
-    <DefaultLayout>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+    <DefaultLayout setLanguage={setLanguage}>
+      {!isLoading ? <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
         {repositories.map((repository) => (
           <div key={repository.id}>
             <CardOne repository={repository} />
           </div>
         ))}
-      </div>
+      </div> : <Spinner />}
     </DefaultLayout>
   );
 };
