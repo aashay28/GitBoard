692dfc5b41b0687593f427674ebd28a81ec30841
aashay shah <aashayshah28@gmail.com>
2023-05-02 12:50:56 +0530

    Ui changes - responsive

--- src/pages/Dashboard/Projects.jsx
+++ src/pages/Dashboard/Projects.jsx
@@ -1,16 +1,16 @@
-import DefaultLayout from "../../layout/DefaultLayout";
-import CardOne from "../../components/CardOne";
-import Spinner from "../../helpers/Spinner";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { languageOptions, sortDirection } from "../../helpers/Constant";
-import DropdownSelect from "../../components/DropdownSelect";
+import DefaultLayout from '../../layout/DefaultLayout';
+import CardOne from '../../components/CardOne';
+import Spinner from '../../helpers/Spinner';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { languageOptions, sortDirection } from '../../helpers/Constant';
+import DropdownSelect from '../../components/DropdownSelect';
 const Projects = () => {
   const [repositories, setRepositories] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
-  const [filterLoading, setFilterLoading] = useState(false)
-  const [language, setLanguage] = useState('python')
-  const [direction, setDirection] = useState('desc')
+  const [isLoading, setIsLoading] = useState(false);
+  const [filterLoading, setFilterLoading] = useState(false);
+  const [language, setLanguage] = useState('python');
+  const [direction, setDirection] = useState('desc');
 
   useEffect(() => {
     setIsLoading(true);
@@ -24,53 +24,62 @@ const Projects = () => {
       })
       .catch((error) => {
         console.log(error);
-        setIsLoading(false)
+        setIsLoading(false);
       });
   }, [language, direction]);
 
   return (
-    <DefaultLayout >
-      <div className="flex gap-8 mb-4 rounded-sm border border-stroke bg-white py-5 px-5 justify-end">
-        {!filterLoading && <>
-          <div>
-            <DropdownSelect
-              setData={setDirection}
-              data={direction}
-              options={sortDirection}
-              label={"Direction"}
-            />
-          </div>
-          <div>
-            <DropdownSelect
-              setData={setLanguage}
-              data={language}
-              options={languageOptions}
-              label={"Language"}
-            />
-          </div>
-          <div>
-            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              onClick={() => {
-                setFilterLoading(true)
-                setTimeout(() => {
-                  setLanguage('python')
-                  setDirection('desc')
-                  setFilterLoading(false)
-                }, 10)
-              }}
-            >Clear Filters</button>
-          </div></>}
+    <DefaultLayout>
+      <div className='flex gap-8 mb-4 rounded-sm border border-stroke bg-white py-5 px-5 justify-end'>
+        {!filterLoading && (
+          <>
+            <div>
+              <DropdownSelect
+                setData={setDirection}
+                data={direction}
+                options={sortDirection}
+                label={'Direction'}
+              />
+            </div>
+            <div>
+              <DropdownSelect
+                setData={setLanguage}
+                data={language}
+                options={languageOptions}
+                label={'Language'}
+              />
+            </div>
+            <div>
+              <button
+                type='button'
+                className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+                onClick={() => {
+                  setFilterLoading(true);
+                  setTimeout(() => {
+                    setLanguage('python');
+                    setDirection('desc');
+                    setFilterLoading(false);
+                  }, 10);
+                }}
+              >
+                Clear
+              </button>
+            </div>
+          </>
+        )}
       </div>
-      {
-        !isLoading ? <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+      {!isLoading ? (
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5'>
           {repositories.map((repository) => (
             <div key={repository.id}>
               <CardOne repository={repository} />
             </div>
           ))}
-        </div> : <Spinner />
-      }
-    </DefaultLayout >
+        </div>
+      ) : (
+        <Spinner />
+      )}
+    </DefaultLayout>
   );
 };
 
