494276aec0256d873e0ce74ee3174cf487988275
aashay shah <aashayshah28@gmail.com>
2023-05-02 11:49:51 +0530

    Updated languages dropdown - tailwind + responsive

--- src/pages/Dashboard/Projects.jsx
+++ src/pages/Dashboard/Projects.jsx
@@ -1,21 +1,21 @@
-import DefaultLayout from "../../layout/DefaultLayout";
-import CardOne from "../../components/CardOne";
-import Spinner from "../../helpers/Spinner";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import DefaultLayout from '../../layout/DefaultLayout';
+import CardOne from '../../components/CardOne';
+import Spinner from '../../helpers/Spinner';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 const Projects = () => {
   const [repositories, setRepositories] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
-  const [language, setLanguage] = useState('python')
+  const [isLoading, setIsLoading] = useState(false);
+  const [language, setLanguage] = useState('javascript');
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     axios
       .get(
         `https://api.github.com/search/repositories?q=language:${language}+stars:>=100&sort=stars&order=desc`
       )
       .then((response) => {
         setRepositories(response.data.items);
-        setIsLoading(false)
+        setIsLoading(false);
       })
       .catch((error) => {
         console.log(error);
@@ -23,14 +23,18 @@ const Projects = () => {
   }, [language]);
 
   return (
-    <DefaultLayout setLanguage={setLanguage}>
-      {!isLoading ? <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        {repositories.map((repository) => (
-          <div key={repository.id}>
-            <CardOne repository={repository} />
-          </div>
-        ))}
-      </div> : <Spinner />}
+    <DefaultLayout setLanguage={setLanguage} language={language}>
+      {!isLoading ? (
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5'>
+          {repositories.map((repository) => (
+            <div key={repository.id}>
+              <CardOne repository={repository} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <Spinner />
+      )}
     </DefaultLayout>
   );
 };
