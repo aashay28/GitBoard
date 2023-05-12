482f370214e1c98250c0221aef5a4312dc0dc946
aashay shah <aashayshah28@gmail.com>
2023-05-12 12:39:47 +0530

    Add links of the repository in profile page

--- src/components/TableThree.jsx
+++ src/components/TableThree.jsx
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import { Link } from 'react-router-dom';
 const TableThree = ({ userData }) => {
   return (
     <div className='rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1'>
@@ -28,7 +29,9 @@ const TableThree = ({ userData }) => {
               <tr key={i}>
                 <td className='border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11'>
                   <h5 className='font-medium text-black dark:text-white'>
-                    {data.name}
+                    <Link target='_blank' to={data.html_url}>
+                      {data.name}
+                    </Link>
                   </h5>
                   <p className='text-sm'>
                     {data.fork === true ? 'Forked' : 'Admin'}
