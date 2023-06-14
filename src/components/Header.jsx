c208a05d57f24e7fb857e716d642fc92cfec1758
yi-heng-chen <yiheng.chen@icloud.com>
2023-06-14 23:03:59 +1000

    dark mode switcher now is fixed on the right side

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = (props) => {
   return (
     <header className='sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none'>
-      <div className='flex flex-grow items-center justify-between py-4 px-4 shadow-2 md:px-6 2xl:px-11 space-x-4'>
+      <div className='flex flex-grow items-center justify-between lg:justify-end py-4 px-4 shadow-2 md:px-6 2xl:px-11'>
         <div className='flex items-center gap-2 sm:gap-4 lg:hidden'>
           {/* <!-- Hamburger Toggle BTN --> */}
           <button
